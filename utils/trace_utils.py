8d91fca0e67c18b2e3b29b0a7b14aac33d6975d3
Alexander Kagan <amkagan@Alexanders-MacBook-Air.local>
2026-07-17 00:11:24 -0400

    notebooks cleaned + small bugs fixed

--- utils/trace_utils.py
+++ utils/trace_utils.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from tqdm import tqdm
-from typing import Union, List
+from typing import Tuple, Union, List
 
 from InfluenceDiffusion.Trace import Traces, Trace
 
@@ -25,15 +25,18 @@ def make_report_traces(traces: Traces, min_edge_participation=5):
     plt.show()
     return edge_activations
 
-    
-def trace_train_test_split(traces: Traces, test_ratio):
+
+def trace_train_test_split(traces: Traces, test_ratio)-> Tuple[Traces, Traces]:
     assert 1 >= test_ratio >= 0
     length_sorted_traces = np.array(sorted(traces, key=lambda trace: len(trace.get_all_activated_vertices())),
                                     dtype=object)
     indices = np.arange(len(traces))
     test_indices = indices[::int(1 / test_ratio)]
     train_indices = list(set(indices) - set(test_indices))
-    return list(length_sorted_traces[train_indices]), list(length_sorted_traces[test_indices])
+    train_traces = list(length_sorted_traces[train_indices])
+    test_traces = list(length_sorted_traces[test_indices])
+    return Traces(traces.graph, train_traces), \
+           Traces(graph=traces.graph, traces=test_traces)
 
 
 def extract_vertex_preactivation_masks(traces: Union[Traces, List[Trace]]):
@@ -53,3 +56,26 @@ def extract_vertex_preactivation_masks(traces: Union[Traces, List[Trace]]):
             vertex_2_t_tm1_masks[vertex][1].append(mask_tm1)
     return {vertex: (np.vstack(masks_t), np.vstack(masks_tm1))
             for vertex, (masks_t, masks_tm1) in vertex_2_t_tm1_masks.items() if len(masks_t) > 0}
+
+
+def compute_new_parent_appearence_times_in_trace(trace: Trace):
+    informative_vs = trace.get_all_failed_and_activated_vertices_no_seed()
+    activ_time_dict = trace.get_activation_time_dict()
+    v_2_new_parent_times = {}
+    for v in informative_vs:
+        v_time = activ_time_dict[v] if v in activ_time_dict else np.inf
+        active_parents = set(trace.graph.get_parents(v)).intersection(activ_time_dict)
+        new_parent_appearence_times = set([activ_time_dict[u] for u in active_parents if activ_time_dict[u] < v_time])
+        v_2_new_parent_times[v] = new_parent_appearence_times
+    return v_2_new_parent_times
+
+
+def compute_vertex_sample_size(traces: Traces):
+    v_2_total_appearences = {v: 0 for v in traces[0].graph.get_sinks()}
+    v_2_num_informative_traces = {v: 0 for v in traces[0].graph.get_sinks()}
+    for trace in traces:
+        v_2_new_parent_times = compute_new_parent_appearence_times_in_trace(trace)
+        for v, new_parent_times in v_2_new_parent_times.items():
+            v_2_total_appearences[v] += len(new_parent_times)
+            v_2_num_informative_traces[v] += 1
+    return v_2_total_appearences, v_2_num_informative_traces
